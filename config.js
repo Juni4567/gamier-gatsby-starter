7b5636eaa801c420d57c52729222d11295c0878f
Junaid <junaid4567@gmail.com>
2019-05-17 15:52:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
-  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
-  siteRss: '/rss.xml',
-  siteFBAppID: '', // FB Application ID for using app insights
-  googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
-}
+  siteTitle: "Welcome to Gamier - a place full of programming ideas and hacks", // Site title.
+  siteTitleAlt: "Gamier", // Alternative site title for SEO.
+  siteLogo: "/icons/icon-512x512.png", // Logo used for SEO and manifest.
+  siteUrl: "https://gamier.netlify.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription:
+    "Welcome to Gamier , we share ideas and free softwares to help each other grow. We at Gamier strive to bring you the smartest takes on topics that matter. We also upload free softwares books and games.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml",
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleTagManagerID: "", // GTM tracking ID.
+  disqusShortname: "gamier", // Disqus shortname.
+  userName: "Junaid Anwar",
+  userTwitter: "Juni4567",
+  userLocation: "Pakistan",
+  userDescription: "",
+  copyright: "Copyright © Gamier 2019. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#00d1b2", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff" // Used for setting manifest background color.
+};
