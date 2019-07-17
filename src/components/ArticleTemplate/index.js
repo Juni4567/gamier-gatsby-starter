15f82b7e0f5db5c6bae7f20bc158e276cc2fc778
Junaid <junaid4567@gmail.com>
2019-07-17 13:43:05 +0500

    Improve the article page

--- src/components/ArticleTemplate/index.js
+++ src/components/ArticleTemplate/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Content from '../Content'
-import { kebabCase } from 'lodash'
-import { Link } from 'gatsby'
+import React from "react";
+import Content from "../Content";
+import { kebabCase } from "lodash";
+import { Link } from "gatsby";
 
 const ArticleTemplate = ({
   content,
@@ -10,32 +10,45 @@ const ArticleTemplate = ({
   meta_title,
   meta_desc,
   tags,
-  title,
+  title
 }) => {
-  const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content;
   return (
     <div>
-      <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
-        {title}
-      </h1>
-      <img src={cover} alt={title} />
-      <PostContent content={content} />
-      <div style={{ marginTop: `4rem` }}>
-        <h4>Tags</h4>
-        <ul className='taglist'>
-          {(tags && tags.length)
-            ? tags.map(tag => (
-              <li key={tag + `tag`}>
-                <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-              </li>
-            ))
-            : null
-          }
-        </ul>
+      <div className="article-header">
+        <div className="columns is-vcentered is-centered">
+          <div className="column is-offset-2">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+          </div>
+          <div className="column is-5">
+            <img className="cover-image" src={cover} alt={title} />
+          </div>
+        </div>
       </div>
-      <hr />
+      <div className="container content article-body">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <PostContent content={content} />
+            <div style={{ marginTop: `4rem` }}>
+              <h4>Tags</h4>
+              <ul className="taglist">
+                {tags && tags.length
+                  ? tags.map(tag => (
+                      <li key={tag + `tag`}>
+                        <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                      </li>
+                    ))
+                  : null}
+              </ul>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ArticleTemplate
+export default ArticleTemplate;
--- src/templates/article-page.js
+++ src/templates/article-page.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {graphql} from 'gatsby'
-import {HTMLContent} from '../components/Content'
-import ArticleTemplate from '../components/ArticleTemplate'
-import SE0 from '../components/SEO'
-import Share from '../components/Share'
-import Disqus from '../components/Disqus'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { HTMLContent } from "../components/Content";
+import ArticleTemplate from "../components/ArticleTemplate";
+import SE0 from "../components/SEO";
+import Share from "../components/Share";
+import Disqus from "../components/Disqus";
+import Layout from "../components/Layout";
 
-const ArticlePage = ({data}) => {
-  const {markdownRemark: post} = data
+const ArticlePage = ({ data }) => {
+  const { markdownRemark: post } = data;
   return (
     <Layout>
-      <section className='section'>
+      <section className="section">
         <SE0
           title={post.frontmatter.title}
           meta_title={post.frontmatter.meta_title}
@@ -21,43 +21,41 @@ const ArticlePage = ({data}) => {
           slug={post.fields.slug}
           date={post.frontmatter.date}
         />
-        <div className='container content'>
-          <div className='columns'>
-            <div className='column is-10 is-offset-1'>
-              <ArticleTemplate
-                content={post.html}
-                contentComponent={HTMLContent}
-                cover={post.frontmatter.cover}
-                meta_title={post.frontmatter.meta_title}
-                meta_desc={post.frontmatter.meta_description}
-                tags={post.frontmatter.tags}
-                title={post.frontmatter.title}
-              />
+
+        <ArticleTemplate
+          content={post.html}
+          contentComponent={HTMLContent}
+          cover={post.frontmatter.cover}
+          meta_title={post.frontmatter.meta_title}
+          meta_desc={post.frontmatter.meta_description}
+          tags={post.frontmatter.tags}
+          title={post.frontmatter.title}
+        />
+        <div className="container content">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
               <Share
                 title={post.frontmatter.title}
                 slug={post.fields.slug}
                 excerpt={post.frontmatter.meta_description}
               />
               <hr />
-              <Disqus
-                title={post.frontmatter.title}
-                slug={post.fields.slug}
-              />
+              <Disqus title={post.frontmatter.title} slug={post.fields.slug} />
             </div>
           </div>
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
 ArticlePage.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
+    markdownRemark: PropTypes.object
+  })
+};
 
-export default ArticlePage
+export default ArticlePage;
 
 export const pageQuery = graphql`
   query ArticleByID($id: String!) {
@@ -65,8 +63,8 @@ export const pageQuery = graphql`
       id
       html
       fields {
-            slug
-          }
+        slug
+      }
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
@@ -77,4 +75,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
