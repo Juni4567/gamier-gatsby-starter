755d3dc0f8a7e78e87d39c61b6fc66826078017b
Junaid <junaid4567@gmail.com>
2019-07-21 16:31:21 +0500

    Updates

--- src/components/Offerings/index.js
+++ src/components/Offerings/index.js
@@ -9,12 +9,12 @@ const Offerings = ({ gridItems }) => (
         className="column is-4"
         style={{ borderRadius: "5px" }}
       >
-        <section className="section">
-          <div className="has-text-centered">
-            <img alt={item.heading} src={item.image} />
+        <section className="section service-module has-text-centered">
+          <div className="">
+            <img className="module-image" alt={item.heading} src={item.image} />
           </div>
-          <h3>{item.heading}</h3>
-          <p>{item.text}</p>
+          <h3 className="module-title">{item.heading}</h3>
+          <p className="module-content">{item.text}</p>
         </section>
       </div>
     ))}
